adc5c3147be59bdd0a8623a1b34037c866a3936e
Tarik Karaca <19tarik97@gmail.com>
2023-11-06 00:40:25 +0100

    feat: init.sh, tmux window index at 1

--- init.sh
+++ init.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+ROOT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
 set -eu
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 # stow
+
+if [ ! -f ~/.tmux.conf ]; then
+	ln -s "$ROOT_DIR/.tmux.conf" ~/.tmux.conf || true
+fi
+
+if [ ! -d ~/config/nvim ]; then
+	ln -s "$ROOT_DIR/.config/nvim" ~/.config/nvim || true
+fi
