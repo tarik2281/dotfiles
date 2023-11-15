86f7466f7566e2447ca65395ca63a735cc93e4e7
Tarik Karaca <19tarik97@gmail.com>
2023-11-15 01:33:04 +0100

    feat: extended jdtls config

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,55 +1,40 @@
+local jdtls = require("jdtls")
 local jdtls_dir = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
 local lombok_jar = jdtls_dir .. "/lombok.jar"
 
-local keymap = vim.keymap -- for conciseness
-local opts = { noremap = true, silent = true }
-local on_attach = function(client, bufnr)
-	opts.buffer = bufnr
-
-	-- set keybinds
-	opts.desc = "Show LSP references"
-	keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-	opts.desc = "Go to declaration"
-	keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-	opts.desc = "Show LSP definitions"
-	keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-	opts.desc = "Show LSP implementations"
-	keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-	opts.desc = "Show LSP type definitions"
-	keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-	opts.desc = "See available code actions"
-	keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-	opts.desc = "Smart rename"
-	keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-	opts.desc = "Show buffer diagnostics"
-	keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-	opts.desc = "Show line diagnostics"
-	keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-	opts.desc = "Go to previous diagnostic"
-	keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-	opts.desc = "Go to next diagnostic"
-	keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-	opts.desc = "Show documentation for what is under cursor"
-	keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-	opts.desc = "Restart LSP"
-	keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-end
+local on_attach = require("usr.core.lspkeymaps").on_attach
 
 local config = {
 	cmd = { jdtls_dir .. "/jdtls", "--jvm-arg=-javaagent:" .. lombok_jar },
-	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+	root_dir = vim.fs.dirname(
+		vim.fs.find({ "gradlew", ".git", "mvnw", "settings.gradle", "pom.xml" }, { upward = true })[1]
+	),
 	on_attach = on_attach,
+	init_options = {
+		extendedClientCapabilities = jdtls.extendedClientCapabilities,
+	},
+	settings = {
+		java = {
+			eclipse = {
+				downloadSources = true,
+			},
+			maven = {
+				downloadSources = true,
+			},
+			implementationsCodeLens = {
+				enabled = true,
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			signatureHelp = {
+				enabled = true,
+			},
+			contentProvider = {
+				preferred = "fernflower",
+			},
+		},
+	},
 }
-require("jdtls").start_or_attach(config)
+
+jdtls.start_or_attach(config)
