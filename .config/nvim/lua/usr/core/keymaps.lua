5ebaab056c4d0479d6d6c4705b7463baa59adb89
Tarik Karaca <19tarik97@gmail.com>
2023-12-01 16:38:50 +0100

    a

--- .config/nvim/lua/usr/core/keymaps.lua
+++ .config/nvim/lua/usr/core/keymaps.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fa", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<CR>", { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Find help" })
@@ -21,6 +22,9 @@ vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle <CR>", { desc = "Toggle NvimTr
 
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
+
+vim.keymap.set("n", "H", "zH", { desc = "Half page left" })
+vim.keymap.set("n", "L", "zL", { desc = "Half page right" })
 
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next occurrence" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous occurrence" })
