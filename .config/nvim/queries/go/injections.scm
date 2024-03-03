1c042106999e20b257bac6962b32b50b7f9018ab
Tarik Karaca <19tarik97@gmail.com>
2024-03-03 22:28:54 +0100

    feat: language injections in strings for go

---
+++ .config/nvim/queries/go/injections.scm
@@ -0,0 +1,27 @@
+(argument_list
+  (comment) @injection.language (#offset! @injection.language 0 3 0 -3)
+  (raw_string_literal) @injection.content)
+
+(var_spec
+  (comment) @injection.language (#offset! @injection.language 0 3 0 -3)
+  (expression_list
+    (raw_string_literal) @injection.content))
+
+(short_var_declaration
+  (comment) @injection.language (#offset! @injection.language 0 3 0 -3)
+  (expression_list
+    (raw_string_literal) @injection.content))
+
+(argument_list
+  (comment) @injection.language (#offset! @injection.language 0 3 0 -3)
+  (interpreted_string_literal) @injection.content)
+
+(var_spec
+  (comment) @injection.language (#offset! @injection.language 0 3 0 -3)
+  (expression_list
+    (interpreted_string_literal) @injection.content))
+
+(short_var_declaration
+  (comment) @injection.language (#offset! @injection.language 0 3 0 -3)
+  (expression_list
+    (interpreted_string_literal) @injection.content))
